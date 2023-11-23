aae36d0415537d46917ad8cfa3ae3518fe6ae66a
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-11-23 21:02:37 +0200

    implement select many with filters

--- LinqExercises/Program.cs
+++ LinqExercises/Program.cs
@@ -6,7 +6,7 @@
         {
             //PersonsDatabase.SaveToXml("Persons.xml");
 
-            PersonsDatabase.ReadFromXml("Persons.xml");
+            // PersonsDatabase.ReadFromXml("Persons.xml");
 
             /*
             var query = from person in PersonsDatabase.AllPersons
@@ -18,19 +18,35 @@
                         };
             */
 
-            var query = PersonsDatabase.AllPersons
-                .Where(person => person.Age >= 50)
-                .Select(person => new
+            int[] set1 = { 1, 2, 3, 4, 5 };
+            int[] set2 = { 9, 8, 7, 6, 5 };
+            // (1, 9), (1, 8), (1, 7), (1, 6), (1, 5)
+            // (2, 9), (2, 8), (2, 7), (2, 6), (2, 5)
+            // (3, 9), (3, 8), (3, 7), (3, 6), (3, 5)
+            // (4, 9), (4, 8), (4, 7), (4, 6), (4, 5)
+
+            IEnumerable<string> query = from elem1 in set1
+                                        from elem2 in set2
+                                        where Math.Abs(elem2 - elem1) == 1
+                                        select $"({elem1},{elem2})";
+
+            IEnumerable<string> query2 = set1.SelectMany(
+                elem1 => set2,
+                (elem1, elem2) => new
                 {
-                    FullName = person.FullName,
-                    DateOfBirth = person.DateOfBirth
-                });
+                    Elem1 = elem1,
+                    Elem2 = elem2
+                })
+                .Where(pair => Math.Abs(pair.Elem2 - pair.Elem1) == 1)
+                .Select(pair => $"({pair.Elem1},{pair.Elem2})");
 
-            foreach (var obj in query)
-            {
-                Console.WriteLine($"{obj.FullName} - {obj.DateOfBirth:yyyy-MM-dd}");
-            }
 
+            foreach (string pairs in query) { Console.Write(pairs + ", "); }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            foreach (string pairs in query2) { Console.Write(pairs + ", "); }
 
             //foreach (Person p in query)
             //{
